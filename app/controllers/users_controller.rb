b4ca71ec08eff22f211f540fae9fabf62ea4878a
Kristian <kristian.lindin@me.com>
2014-04-07 15:56:28 +0200

    Finish user: index, edit, update and destroy actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,11 @@
 class UsersController < ApplicationController
+  before_action :signed_in_user, only: [:index, :edit, :update]
+  before_action :correct_user, only: [:edit, :update]
+  before_action :admin_user, only: [:destroy]
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
 
   def show
   	@user = User.find(params[:id])
@@ -19,9 +26,66 @@ class UsersController < ApplicationController
   	end
   end
 
-  private
-  def user_params
-  	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  def edit
+    @user = User.find(params[:id])
   end
 
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile Updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    User.find_by(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
+
+  private
+    def user_params
+    	params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    end
+
+    #before filters
+
+    def signed_in_user
+      unless signed_in?
+        store_location
+        redirect_to signin_url, notice: "Please sign in."
+        #same as:
+        #  unless signed_in? 
+        #     flash[:notice] = "Please sign in."
+        #     redirect_to signin_url
+        #  end
+      end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to root_url unless current_user?(@user)
+    end
+
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- app/models/user.rb
+++ app/models/user.rb
@@ -10,7 +10,7 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 
 	has_secure_password
-	validates :password, length: { minimum: 6 }
+	validates :password, length: { minimum: 6 }, presence: true
 	
 
 
